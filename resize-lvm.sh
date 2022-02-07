fe385d264bc888c5625b5d687d3abf04cb9a92b5
oleg1942 <59996135+oleg1942@users.noreply.github.com>
2022-02-07 16:29:39 +0600

    Create resize-lvm.sh

---
+++ resize-lvm.sh
@@ -0,0 +1,16 @@
+echo "EXTENDED partition resize"
+parted /dev/sda resizepart 2 100% >/dev/null
+sleep 5
+echo "LVM partition resize"
+parted /dev/sda resizepart 5 100% >/dev/null
+sleep 5
+echo "PV partition resize"
+pvresize /dev/sda5 >/dev/null
+sleep 4
+echo "LVM partition resize"
+lvextend -l +100%FREE /dev/mapper/debian--vg-root >/dev/null
+sleep 2
+echo "Debian-ROOT partition resize"
+resize2fs /dev/mapper/debian--vg-root >/dev/null
+df -h
+echo "DONE!!! by Oleg1942"
